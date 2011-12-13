0df227fdc32b5cf691e0b236f72aaa6b250a8e9a
FK2703 <fk2703@gmail.com>
2011-12-14 02:28:38 +0300

    First application`s push

---
+++ src/test/project/com/TestprojectActivity.java
@@ -0,0 +1,11 @@
+package test.project.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestprojectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    }
+}
