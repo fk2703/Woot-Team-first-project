6cef3955ee94d0c416536dab01db0d2d04d90e71
FK2703 <fk2703@gmail.com>
2011-12-14 02:31:33 +0300

    "Hello world" test phrase

--- src/test/project/com/TestprojectActivity.java
+++ src/test/project/com/TestprojectActivity.java
@@ -2,10 +2,15 @@ package test.project.com;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TestprojectActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    }
+	   /** Called when the activity is first created. */
+	   @Override
+	   public void onCreate(Bundle savedInstanceState) {
+	       super.onCreate(savedInstanceState);
+	       TextView tv = new TextView(this);
+	       tv.setText("Hello, All\nNew string!");
+	       setContentView(tv);
+	   }
 }
